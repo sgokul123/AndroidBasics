0f46d2c5e4df84e877c72472948cac0c034e1f5d
mukesh <muksh093@gmail.com>
2017-02-25 12:32:59 +0530

    swip done

---
+++ SwipTabEx/app/src/main/java/com/bridgelabz/swiptabex/PagerAdapter.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.swiptabex;
+
+/**
+ * Created by bridgeit on 25/2/17.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                TabFragment1 tab1 = new TabFragment1();
+                return tab1;
+            case 1:
+                TabFragment2 tab2 = new TabFragment2();
+                return tab2;
+            case 2:
+                TabFragment3 tab3 = new TabFragment3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
